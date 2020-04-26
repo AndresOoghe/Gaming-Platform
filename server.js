4772411e4332dea4a2d35ad5449ef4fa531f7701
Andres <andres.ooghe@gmail.com>
2020-04-26 15:13:26 +0200

    [TASK] - Tournament inscriptions are based on userId, not on username

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ require('./config/passport')(passport);
 // EJS
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
+app.set('layout', 'layouts/general');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
